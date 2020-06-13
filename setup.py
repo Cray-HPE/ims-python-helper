3a9d26770412189e732b5cedb42e2b35c007d0e9
Jeff Keopp <keopp@cray.com>
2020-06-12 20:48:12 -0500

    Changes for release branching

---
+++ setup.py
@@ -0,0 +1,32 @@
+# setup.py for ims-python-helper
+# Copyright 2018, 2019, Cray Inc. All Rights Reserved.
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('.rpm_version', 'r') as fh:
+    version = fh.read().strip()
+
+setuptools.setup(
+    name="ims-python-helper",
+    version=version,
+    install_requires=[
+      'ars-python-helper',  # CASM-1453: remove when epic merged to master
+      'boto3',
+      'oauthlib',
+      'requests',
+      'requests-oauthlib',
+    ],
+    author="Cray Inc.",
+    author_email="sps@cray.com",
+    description="Utility for interacting with the Cray Image Management Service",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://stash.us.cray.com/projects/SCMS/repos/ims-python-helper/browse",
+    packages=setuptools.find_packages(),
+    keywords="Cray IMS",
+    classifiers=(
+      "Programming Language :: Python :: 3.6",
+    ),
+)
